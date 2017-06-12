29129a7b2dafef495b0ad4e1fef9e20cef16b6d4
Aman Jain <aman.jain@innoraft.com>
2017-06-12 11:08:29 +0530

    Setup basic requirement.

--- template.php
+++ template.php
@@ -3,3 +3,11 @@
  * @file
  *   Template File
  */
+
+/**
+ * Implements template_preprocess_page(&$variables).
+ */
+function delight_preprocess_page(&$variables) {
+  $directory = drupal_get_path('theme', 'delight');
+  kpr($variables);
+}
